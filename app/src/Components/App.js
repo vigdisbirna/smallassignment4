f881b1e977d4e00604d70f66c8a18916b9bc4b0f
Vigdís Birna Þorsteinsdóttir <vigdis16@ru.is>
2019-03-31 15:30:45 +0000

    creating react app

---
+++ app/src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../App.css";
+
+import Modal from "./Modal/Modal";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Modal />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/src/Components/Modal/Modal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Modal extends React.Component {
+  render() {
+    return <div>hihi</div>;
+  }
+}
+
+export default Modal;
